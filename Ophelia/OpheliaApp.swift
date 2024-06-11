98f7f4e572aaca7271726b06b2b4e7157ccb1d27
Eden <64953425+Solaireis@users.noreply.github.com>
2024-06-11 23:16:10 +0800

    Initial Commit

---
+++ Ophelia/OpheliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpheliaApp.swift
+//  Ophelia
+//
+//  Created by Gwen Synthea Jing Xuan  on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct OpheliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpheliaTests/OpheliaTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpheliaTests.swift
+//  OpheliaTests
+//
+//  Created by Gwen Synthea Jing Xuan  on 11/6/24.
+//
+
+import Testing
+@testable import Ophelia
+
+struct OpheliaTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
